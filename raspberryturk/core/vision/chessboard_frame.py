faa99338e12f444165d7da91cf3a643c3e937474
dzhelyazkov <d.zhelyazkov@ocado.com>
2023-05-05 11:49:18 +0300

    dev: board vision analysis

--- raspberryturk/core/vision/chessboard_frame.py
+++ raspberryturk/core/vision/chessboard_frame.py
@@ -1,5 +1,7 @@
+import cv2
+
+from .constants import SQUARE_SIZE, BOARD_SIZE, M, ROI
 from .square import Square
-from .constants import SQUARE_SIZE, BOARD_SIZE
 
 
 class ChessboardFrame():
@@ -9,4 +11,11 @@ class ChessboardFrame():
     def square_at(self, i):
         y = BOARD_SIZE - (int(i / 8) % 8) * SQUARE_SIZE - SQUARE_SIZE
         x = (i % 8) * SQUARE_SIZE
-        return Square(i, self.img[y:y+SQUARE_SIZE, x:x+SQUARE_SIZE, :])
+        return Square(i, self.img[y:y + SQUARE_SIZE, x:x + SQUARE_SIZE])
+
+
+def get(img_read):
+    img_roi = img_read[ROI[0]:ROI[1], ROI[2]:ROI[3]]
+    img_captured = cv2.resize(img_roi, (BOARD_SIZE, BOARD_SIZE))
+    warped = cv2.warpPerspective(img_captured, M, (BOARD_SIZE, BOARD_SIZE))
+    return ChessboardFrame(warped)
--- raspberryturk/core/vision/constants.py
+++ raspberryturk/core/vision/constants.py
@@ -1,2 +1,15 @@
-SQUARE_SIZE = 60
-BOARD_SIZE = SQUARE_SIZE * 8
+import cv2
+import numpy as np
+
+BOARD_SIZE = 800
+SQUARE_SIZE = int(BOARD_SIZE / 8)
+
+ROI = (350, 1350, 900, 2100)
+
+top_left = [[95, 60]]  # bottom right[]y,x
+bottom_left = [[18, 735]]  # bottom left
+top_right = [[675, 83]]  # top right
+bottom_right = [[730, 750]]  # y delta olnly first column
+pts1 = np.float32([top_left, bottom_right, top_right, bottom_left, ])
+pts2 = np.float32([[0, 0], [BOARD_SIZE, BOARD_SIZE], [BOARD_SIZE, 0], [0, BOARD_SIZE], ])
+M = cv2.getPerspectiveTransform(pts1, pts2)
